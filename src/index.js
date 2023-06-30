75648a0d6d92312fb401d9d8ac5f2604748e2610
Asylbek Ibrakhimov <112480502+asyke@users.noreply.github.com>
2023-06-29 21:53:24 -0500

    finish commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
     domain={process.env.REACT_APP_AUTH_DOMAIN}
     clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: window.location.origin,
+      redirect_url: window.location.origin,
     }}>
     <UserProvider>
       <ProductsProvider>
